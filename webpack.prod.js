f71a9c121fae4a76e72b606be49c90ec05b64181
Steven Souto <stevensouto77@gmail.com>
2024-06-17 21:26:38 +0100

    https

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,7 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new SitemapPlugin('http://blog.stevensouto.com', sitemapPaths),
+    new SitemapPlugin('https://blog.stevensouto.com', sitemapPaths),
     new webpack.EnvironmentPlugin(['GOOGLE_MAPS_API_KEY']),
   ],
   optimization: {
